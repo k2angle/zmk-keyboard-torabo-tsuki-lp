8c1117f653376fec63307c7a242d0aea4c431d2c
k2angle <k2angle@gmail.com>
2026-04-13 19:22:44 +0900

    Update mini_trackpad_iqs7211e_init.h

--- src/mini_trackpad_iqs7211e_init.h
+++ src/mini_trackpad_iqs7211e_init.h
@@ -109,13 +109,13 @@
 #define X_RESOLUTION_1                           0x00 // 0x00E1 = 225
 #define Y_RESOLUTION_0                           0x96 //
 #define Y_RESOLUTION_1                           0x00 // 0x012C = 300
-#define XY_DYNAMIC_FILTER_BOTTOM_SPEED_0         0x30
+#define XY_DYNAMIC_FILTER_BOTTOM_SPEED_0         0x06
 #define XY_DYNAMIC_FILTER_BOTTOM_SPEED_1         0x00
-#define XY_DYNAMIC_FILTER_TOP_SPEED_0            0xF0
+#define XY_DYNAMIC_FILTER_TOP_SPEED_0            0x7C
 #define XY_DYNAMIC_FILTER_TOP_SPEED_1            0x00
 #define XY_DYNAMIC_FILTER_BOTTOM_BETA            0x10
-#define XY_DYNAMIC_FILTER_STATIC_FILTER_BETA     0x02
-#define STATIONARY_TOUCH_MOV_THRESHOLD           0x20
+#define XY_DYNAMIC_FILTER_STATIC_FILTER_BETA     0x07
+#define STATIONARY_TOUCH_MOV_THRESHOLD           0x80
 #define FINGER_SPLIT_FACTOR                      0x00 // 0x00 = 1 finger mode
 #define X_TRIM_VALUE                             0x14
 #define Y_TRIM_VALUE                             0x14
